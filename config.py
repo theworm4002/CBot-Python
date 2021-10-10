6a41af8f73fce33b8778cad2a027c2b035730d8c
ComputerTech <60820431+ComputerTech312@users.noreply.github.com>
2021-10-10 01:47:47 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,47 @@
+###############################################################################
+#    _____ ____        _   
+#   / ____|  _ \      | |                  
+#  | |    | |_) | ___ | |_ 
+#  | |    |  _ < / _ \| __|
+#  | |____| |_) | (_) | |_ 
+#   \_____|____/ \___/ \__|
+#
+#################################################
+# Release: v0.0.1 Python
+#
+#################################################
+
+### Start of configuration ###
+
+
+### Basic requirements.
+
+## Bot nick.
+BNICK = 'CBot'
+
+## Bot ident.
+BIDENT = 'CBot'
+
+## Bot realname.
+BREAL = 'CBot'
+
+## Bot server.
+BSERVER = 'irc.address.org'
+
+## Bot port.
+BPORT = '+6697'
+
+## Bot admin.
+BADMIN = 'Your-nick'
+
+
+### SASL
+
+## Username
+USERNAME = 'CBot'
+
+## Password
+PASSWORD = 'Password'
+
+
+### End of configuration ###
