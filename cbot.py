19fb07b6c6651a564ae6256623428a1962755ee4
ComputerTech <60820431+ComputerTech312@users.noreply.github.com>
2021-10-16 02:47:47 +0100

    Update cbot.py

--- cbot.py
+++ cbot.py
@@ -1,22 +1,31 @@
+#!/usr/bin/python
+
+NICK = "CBot"
+ALT = "CBot-"
+IDENT = "CBot"
+REALNAME = "CBot"
+SERVER = "irc.technet.xi.ht"
+PORT = 6667
+CHANNEL = "#test"
 
 import socket
-import tls
+import sys
 import string
-import config
+import time
 
-  sockChan = socket.socket()
-  if PORT[0][0] == "+": 
-    sockChan = ssl.wrap_socket(s)
+sockChan = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-  sockChan.connect((BHOST, BPORT))
-  sockChan.send('NICK',BNICK)
-  sockChan.send('USER',BIDENT,BNAME))
+sockChan.connect((SERVER, PORT))
+sockChan.send('NICK CBot\r\n'.encode('UTF-8'))
+sockChan.send('USER CBot * * :CBot\r\n'.encode('UTF-8'))
 
-  while 1:
-     text = sockChan.recv(1024)
-     text = string.split(text)
-     
-  if text.find('PING') == [0]:
-      sockChan.send('PONG',text[1])
-
-      
+while 1: 
+    text = sockChan.recv(2040)
+    print(text)
+    foo = text.split()
+    if 'PING' in foo[0]:
+        sockChan.send("PONG " + foo[1] + "\r\n")
+    elif '001' in foo[1]:
+        sockChan.send("JOIN " + CHANNEL + "\r\n")
+    elif '433' in foo[1]:
+        sockChan.send("NICK " + ALT + "\r\n")
