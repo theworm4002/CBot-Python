104a23ba941b57ad04e093b3665e03ca8f236c25
ComputerTech <60820431+ComputerTech312@users.noreply.github.com>
2021-10-17 06:55:08 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,41 @@
+###############################################################################
+#    _____ ____        _   
+#   / ____|  _ \      | |                  
+#  | |    | |_) | ___ | |_ 
+#  | |    |  _ < / _ \| __|
+#  | |____| |_) | (_) | |_ 
+#   \_____|____/ \___/ \__|
+#
+#################################################
+# Description:
+#
+#
+#################################################
+
+### Start of configuration ###
+
+## Bot nick.
+BNICK = "CBot"
+
+## Bot alter nick.
+BALT = "CBot-"
+
+## Bot ident.
+BIDENT = "CBot"
+
+## Bot realname.
+BREALNAME = "CBot"
+
+## Bot server.
+BSERVER = "irc.libera.chat"
+
+## Bot port.
+BPORT = 6667
+
+## Bot admin.
+BADMIN = "Your-nick"
+
+## Bot channel.
+BCHANNEL = "#CBot"
+
+### End of configuration ###
